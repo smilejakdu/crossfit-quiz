da75c6794b8a0b3f1434e474bd9fa23a3dc013eb
smilejakdu <ash982416@gmail.com>
2021-07-06 17:56:32 +0900

    first back settings

---
+++ back/app.js
@@ -0,0 +1,39 @@
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var cors = require("cors");
+var config = require("./config")
+var indexRouter = require("./routes/index");
+
+
+var app = express();
+
+// settings
+app.use(cors());
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// routes
+app.use("/", indexRouter); // 127.0.0.1:3000
+
+
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+app.use(function (err, req, res, next) {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+module.exports = app;
